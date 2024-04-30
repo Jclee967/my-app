1811b2855383529b20ae4f200ddbf3afd5c505e0
Jessica Lee <jessicalee936@gmail.com>
2024-04-30 19:12:45 +0000

    Initial commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import {Tabs, Redirect} from 'expo-router'
+
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs>
+        <Tabs.Screen name="home" options={{title: 'Home'}} />
+        <Tabs.Screen name="profile" options={{title: 'Profile'}} />
+      </Tabs>
+    </>
+  )
+}
+
+export default TabsLayout
